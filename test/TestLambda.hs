5b00a59e0791d096af6dffa915fffab104200f4d
Your Name <you@example.com>
2020-08-06 12:25:57 +0300

    refactor to λ

--- test/TestLambda.hs
+++ test/TestLambda.hs
@@ -10,7 +10,7 @@ data LExpr
 show' :: LExpr -> String
 show' (Variable a) = a
 show' (Apply e1 e2) = "(" ++ show' e1 ++ " " ++ show' e2 ++ ")"
-show' (Lambda x e) = "(\\" ++ x ++ " -> " ++ show' e ++ ")"
+show' (Lambda x e) = "(λ " ++ x ++ " -> " ++ show' e ++ ")"
 
 instance Show LExpr where
   show = show'
@@ -21,6 +21,7 @@ main = defaultMain alltests
 alltests :: TestTree
 alltests = testGroup "all" [mvptests, extendedtests]
 
+
 mvptests :: TestTree
 mvptests =
   testGroup
@@ -29,21 +30,23 @@ mvptests =
         show (Variable "x") @?= "x",
       testCase "application" $
         show (Apply (Variable "x") (Variable "y")) @?= "(x y)",
-      testCase "application" $
-        show (Lambda "x" (Variable "e")) @?= "(\\x -> e)"
+      testCase "abstraction" $
+        show (Lambda "x" (Variable "e")) @?= "(λ x -> e)"
     ]
 
 extendedtests :: TestTree
 extendedtests =
   testGroup
     "all examples"
-    [ testCase "..." $
-        show (Apply (Lambda "x" (Variable "x")) (Variable "y")) @?= "((\\x -> x) y)",
-      testCase "..." $
-        show (Lambda "x" (Apply (Variable "y") (Variable "z"))) @?= "(\\x -> (y z))",
-      testCase "..." $
-        show (Lambda "x" (Lambda "y" (Variable "z"))) @?= "(\\x -> (\\y -> z))",
-      testCase "..." $
-        show (Apply (Apply (Variable "a") (Variable "b")) (Variable "c")) @?= "((a b) c)"   
+    [ testCase "(λ x -> x) y" $
+        show (Apply (Lambda "x" (Variable "x")) (Variable "y")) @?= "((λ x -> x) y)",
+      testCase "(λ x -> y z)" $
+        show (Lambda "x" (Apply (Variable "y") (Variable "z"))) @?= "(λ x -> (y z))",
+      testCase "λ x -> (λ y -> z)" $
+        show (Lambda "x" (Lambda "y" (Variable "z"))) @?= "(λ x -> (λ y -> z))",
+      testCase "(a b c)" $
+        show (Apply (Apply (Variable "a") (Variable "b")) (Variable "c")) @?= "((a b) c)"
+--      ,testCase "last step: λ x y -> z" $
+--        show (Lambda "x" (Lambda "y" (Variable "z"))) @?= "λ x y -> z"
     ]
 
